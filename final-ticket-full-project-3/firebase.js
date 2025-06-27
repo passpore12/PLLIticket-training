6d5cd62fbe6c6353199ea3c1cc250c13ed936215
passpore12 <combers-62sate@icloud.com>
2025-06-27 14:21:16 +0800

    Update firebase.js

--- final-ticket-full-project-3/firebase.js
+++ final-ticket-full-project-3/firebase.js
@@ -1,14 +1,12 @@
-// firebase.js（非模組語法，v9 compatibility）
+// firebase.js（compat 非模組寫法）
 const firebaseConfig = {
-  apiKey: "AIzaSyCyBhpl_IBBLrcTY9ky48kKfhTem0QzSG8",
-  authDomain: "plli-tickettraing.firebaseapp.com",
-  projectId: "plli-tickettraing",
-  storageBucket: "plli-tickettraing.firebasestorage.app",
-  messagingSenderId: "191830693744",
-  appId: "1:191830693744:web:84511cf3c1b30b172fe1b5",
-  measurementId: "G-M41613HPD1"
+  apiKey: "AIzaSyCkFJqLLJRt8PBL7001ZxoLtIYtDoLFPfw",
+  authDomain: "plli-tickettraining.firebaseapp.com",
+  projectId: "plli-tickettraining",
+  storageBucket: "plli-tickettraining.appspot.com",
+  messagingSenderId: "954271429129",
+  appId: "1:954271429129:web:df6ca28e3437d3d7853ed8"
 };
 
-// 初始化 Firebase（使用 compat 模式）
 firebase.initializeApp(firebaseConfig);
-db = firebase.firestore(); // ✅ 設為全域變數，讓其他檔案可使用
+db = firebase.firestore(); // ✅ 設為全域變數
